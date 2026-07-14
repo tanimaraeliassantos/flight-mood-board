4ee511dfa38fb18a09138bd95dd896247c91932a
Tanimara Elias Santos <tanimaraeliassantos@gmail.com>
2026-07-14 12:41:51 +0200

    build: set up Webpack configuration for Vue.js development
    - add vueloaderplugin and htmlwebpackplugin for vue single-file component support
    - configure entry point, output path, and clean build directory
    - set up loaders for .vue, js and scss files
    - add resolve extensions and alias for Vue ES module bundler
    - add resolve extensions and alias for Vue ES module bundler
    - configure devserver with hot reload on port 8080

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const { VueLoaderPlugin } = require('vue-loader');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+	mode: 'development',
+	entry: './src/main.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js',
+		clean: true,
+	},
+	plugins: [
+		new VueLoaderPlugin(),
+		new HtmlWebpackPlugin({
+			template: './src/index.html',
+		}),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.vue$/,
+				loader: 'vue-loader',
+			},
+			{
+				test: /\.js$/,
+				loader: 'babel-loader',
+			},
+			{
+				test: /\.scss$/,
+				use: ['style-loader', 'css-loader', 'sass-loader'],
+			},
+		],
+		resolve: {
+			extensions: ['.js', '.vue', '.json'],
+			alias: {
+				vue$: 'vue/dist/vue.esm-bundler.js',
+			},
+		},
+		devServer: {
+			port: 8080,
+			hot: true,
+			open: true,
+		},
+	},
+};
